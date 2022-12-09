f8c170b4545edc2dea3b7ede41ef5c12ff5f9651
Alberto Romero Abarca <55192337+LordArner@users.noreply.github.com>
2022-12-09 18:06:06 +0100

    Añadida base de datos

---
+++ lib/dataModel.dart
@@ -0,0 +1,54 @@
+///Aqui se definen los strings con los nombres para las tablas de la DB
+final String paisesFavs = 'favs';
+final String paisesVisitados = 'visited';
+final String paisesPlan = 'planned';
+
+///Guarda el string con el nombre de los atributos que usará SQLite en las columnas
+class atributosPais{
+  static final List<String> values = [
+    id, ranking, code
+  ];
+
+  static final String id = '_id';
+  static final String ranking = 'ranking';
+  static final String code = 'code';
+}
+
+///Clase pais, guarda int id, string code (ej: ES, UK, US) y el int ranking
+class Pais{
+  final int? id;
+  final int ranking;
+  final String code;
+
+  const Pais({
+    this.id,
+    required this.ranking,
+    required this.code,
+  });
+
+  ///Convierte el mapa guardado por la DB a un objeto Pais
+  static Pais fromJson(Map<String, Object?> json) => Pais(
+    id: json[atributosPais.id] as int?,
+    ranking: json[atributosPais.ranking] as int,
+    code: json[atributosPais.code] as String,
+  );
+
+  ///Convierte el objeto Pais a un mapa que puede ser leido por la DB
+  Map<String, Object?> toJson() =>{
+    atributosPais.id: id,
+    atributosPais.ranking: ranking,
+    atributosPais.code: code,
+  };
+
+  ///Hace una copia del objeto Pais desde el que se llama
+  Pais copy({
+    int? id,
+    int? ranking,
+    String? code,
+  }) =>
+      Pais(
+        id: id ?? this.id,
+        ranking: ranking ?? this.ranking,
+        code: code ?? this.code,
+      );
+}
---
+++ lib/database.dart
@@ -0,0 +1,125 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:countrylist/dataModel.dart';
+
+
+class PaisDatabase{
+  static final PaisDatabase instance = PaisDatabase._init();
+
+  static Database? _database;
+
+  ///Comienza la inicializacion de la DB
+  PaisDatabase._init();
+
+  ///Intenta abrir una DB local existente y si no la encuentra, la crea con el nombre paises.db
+  Future<Database> get database async{
+    if(_database != null ) return _database!;
+
+    _database = await _initDB('paises.db');
+    return database!;
+  }
+
+  ///Crea una DB con el nombre indicado en la dirección por defecto
+  Future<Database> _initDB(String filePath) async{
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  ///Inicializa la DB con las tablas indicadas
+  Future _createDB(Database db, int version) async {
+    final idType = 'INTEGER PRIMARY KEY AUTONICREMENT';
+    final boolType = 'BOOLEAN NOT NULL';
+    final intType = 'INTEGER NOT NULL';
+    final textType = 'TEXT NOT NULL';
+
+    await db.execute('''
+    CREATE TABLE $paisesFavs(
+    ${atributosPais.id} $idType,
+    ${atributosPais.ranking} $textType,
+    ${atributosPais.code} $intType,
+    )
+    ''');
+
+    await db.execute('''
+    CREATE TABLE $paisesVisitados(
+      ${atributosPais.id} $idType,
+      ${atributosPais.ranking} $textType,
+      ${atributosPais.code} $intType,
+    )
+    ''');
+
+    await db.execute('''
+    CREATE TABLE $paisesPlan(
+    ${atributosPais.id} $idType,
+    ${atributosPais.ranking} $textType,
+    ${atributosPais.code} $intType,
+    )
+    ''');
+  }
+
+  ///Añade un pais a la DB en la tabla indicada
+  Future<Pais> create(Pais note, String table) async{
+    final db = await instance.database;
+
+    final id = await db.insert(table, note.toJson());
+    return note.copy(id: id);
+  }
+
+  ///Lee el pais, si existe, que coincida con la tabla y codigo indicados
+  Future<Pais> read(String code, String table) async{
+    final db = await instance.database;
+
+    final maps = await db.query(
+      table,
+      columns: atributosPais.values,
+      where: '${atributosPais.code} = ?',
+      whereArgs: [code],
+    );
+
+    if (maps.isNotEmpty){
+      return Pais.fromJson(maps.first);
+    }else{
+      throw Exception ('Code $code not found');
+    }
+  }
+
+  ///Lee todas las entradas de la tabla indicada
+  Future<List<Pais>> readAll(String table) async{
+    final db = await instance.database;
+
+    final result = await db.query(table);
+
+    return result.map((json) => Pais.fromJson(json)).toList();
+  }
+
+  ///Actualiza la entrada de la DB que tenga el mismo código, reemplazandola
+  Future<int> update(Pais pais) async{
+    final db = await instance.database;
+
+    return db.update(
+      paisesFavs,
+      pais.toJson(),
+      where: '${atributosPais} = ?',
+      whereArgs: [pais.code],
+    );
+  }
+
+  ///Elimina la entrada de la DB que tenga el código condicado
+  Future<int> delete(String code) async{
+    final db = await instance.database;
+
+    return await db.delete(
+      paisesFavs,
+      where: '${atributosPais} = ?',
+      whereArgs: [code],
+    );
+  }
+
+  ///Cierra la DB
+  Future close() async{
+    final db = await instance.database;
+    db.close();
+  }
+}
